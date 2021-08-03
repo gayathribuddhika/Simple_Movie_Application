5ccf21d8af983e87ff6ab8a23de98a10d201b96e
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-08-03 10:29:43 +0530

    create all the CRUD operations roughly

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -3,4 +3,25 @@ module.exports = class API {
     static async fetchAllPosts(req, res) {
         res.send("Hello from API");
     }
+
+    // fetch posts by ID
+    static async fetchPostById(req, res) {
+        res.send("Fetch post by ID");
+    }
+
+    // create a post
+    static async createPost(req, res) {
+        res.send("create a post");
+    }
+
+    // update a post
+    static async updatePost(req, res) {
+        res.send("Update a post");
+    }
+
+    // delete a post
+    static async deletePost(req, res) {
+        res.send("Delete a post");
+    }
+
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,8 +3,10 @@ const express = require("express");
 const router = express.Router();
 const API = require("../controllers/api");
 
-
-
 router.get("/", API.fetchAllPosts);
+router.get("/:id", API.fetchPostById);
+router.post("/", API.createPost);
+router.patch("/:id", API.updatePost);
+router.delete("/:id", API.deletePost);
 
 module.exports = router;
