4fdf8db538bc2ae86717f840303bddbd1bda9c41
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-08-03 09:13:35 +0530

    database connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 // import
-require('dotenv').config();
-const express = require ("express");
-const cors = require ("cors");
-const mongoose = require ("mongoose");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +10,21 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("uploads"));
 
 // database connection
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Database Connected !!"))
+  .catch((err) => console.log(err));
+
+// start the server
+app.listen(port, () =>
+  console.log(`Server is running at http://localhost:${port}`)
+);
