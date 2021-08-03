d590fb2b805db8bf45d274928bf4e2e8e9261adf
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-08-03 10:53:09 +0530

    create fetch data from db

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,7 +1,14 @@
+const Post = require("../models/posts");
+
 module.exports = class API {
     // fetch all posts
     static async fetchAllPosts(req, res) {
-        res.send("Hello from API");
+        try {
+            const posts = await Post.find();
+            res.status(200).json(posts);            
+        } catch (err) {
+            res.status(404).json({msg: err.msg});
+        }
     }
 
     // fetch posts by ID
