8d3114d9d4ea8e6079ec0f3d18336388be2a3280
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-08-22 11:59:09 +0530

    design home page

---
+++ client/src/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+const url = "api/post";
+
+export default class API {
+    // get all the posts from the server
+    static async getAllPost() {
+        const res = await axios.get(url);
+        return res.data;
+    }
+    // get single post by id
+    static async getPostByID(id) {
+        const res = await axios.get(`${url}/${id}`);
+        return res.data;
+    }
+    // insert post to database
+    static async addPost(post) {
+        const res = await axios.get(url, post);
+        return res.data;
+    }
+    // update post in database
+    static async updatePost(id, post) {
+        const res = await axios.patch(`${url}/${id}`, post);
+        return res.data;
+    }
+    // delete a post
+    static async deletePost(id) {
+        const res = await axios.delete(`${url}/${id}`);
+        return res.data;
+    }
+}
