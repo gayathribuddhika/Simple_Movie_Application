df71993930ad23e8cc6a999c07b8c2cd01679328
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-08-22 21:10:54 +0530

    Create the home page content

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col sm ="4" class="pa-3" v-for="post in posts" :key="post_id">
+    <v-row no-gutters>
+      <v-col sm="4" class="pa-3" v-for="post in posts" :key="post_id">
         <v-card class="pa-1">
           <v-img height="250" :src="`/${post.image}`"></v-img>
-          <v-btn class="ml-4 mt-3" small outlined color="indigo"> {{post.category}}</v-btn>
+          <v-btn class="ml-4 mt-3" small outlined color="indigo">
+            {{ post.category }}</v-btn
+          >
+          <v-card-title class="headline">{{ post.title }}</v-card-title>
+          <v-card-text>
+            <p>{{ post.content.substring(0, 100) + "..." }}</p>
+          </v-card-text>
         </v-card>
-        <v-card-title class="headline">{{post.title}}</v-card-title>
       </v-col>
     </v-row>
   </v-container>
@@ -14,16 +19,16 @@
 
 <script>
 import API from "../api";
-  
-  export default {
-    name: 'Home',
-    data () {
-      return {
-        posts: []
-      };
-    },
-    async created() {
-      this.posts = await API.getAllPost();
-    }
-  }
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  async created() {
+    this.posts = await API.getAllPost();
+  },
+};
 </script>
